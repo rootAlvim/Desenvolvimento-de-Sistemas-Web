d0b750b20b0e01b78e630e1a1834b25cfe50aa05
Álvaro  Silva <alvaro.chaves@escolar.ifrn.edu.br>
2026-09-12 18:54:28 -0300

    Corrigindo Venv

--- Exercicios_Javascript/001.js
+++
@@ -1,8 +0,0 @@
-function duration(x) {
-    let hora = Math.trunc(x / 3600);
-    let minutos = Math.trunc((x - (hora * 3600)) / 60);
-    let segundos = (x - (hora * 3600)) - (Math.trunc((x - (hora * 3600)) / 60) * 60)
-
-    return `${hora}h ${minutos}min ${segundos}s`;
-}
-
