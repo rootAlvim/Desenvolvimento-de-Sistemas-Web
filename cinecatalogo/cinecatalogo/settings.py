acae25eaa65cdd884e8519a100cf001945d107e2
Álvaro  Silva <alvaro.chaves@escolar.ifrn.edu.br>
2026-09-14 08:02:40 -0300

    STATIC_URL e STATICFILES_DIRS no Django

--- cinecatalogo/cinecatalogo/settings.py
+++ cinecatalogo/cinecatalogo/settings.py
@@ -115,9 +115,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / 'cinecatalogo' / 'static',
 
+]
 # Email
 # https://docs.djangoproject.com/en/6.1/topics/email/#topic-email-configuration
 
