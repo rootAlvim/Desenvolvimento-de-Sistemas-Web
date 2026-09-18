474d64aaf9cb8b54ca31b7fe25d50343c4b1c571
Álvaro  Silva <alvaro.chaves@escolar.ifrn.edu.br>
2026-09-18 08:56:42 -0300

    makemigrations e migrate - Aplicando as migrações

--- cinecatalogo/filmes/models.py
+++ cinecatalogo/filmes/models.py
@@ -1,3 +1,21 @@
 from django.db import models
-
+from django .contrib.auth.models import User
 # Create your models here.
+class Categoria(models.Model):
+    ...
+
+
+class Filme(models.Model):
+    title = models.CharField(max_length=50)
+    sinopse = models.TextField(max_length=175)
+    slug = models.SlugField()
+    ano_lancamento = models.IntegerField()
+    diretor = models.CharField(max_length=100)
+    duracao = models.IntegerField()
+    imagem_capa = models.ImageField(upload_to='filmes/capas/%Y/%m/%d/')
+    criado_em = models.DateTimeField(auto_now_add=True) #gera a dat no momento da criação
+    atualizado_em = models.DateTimeField(auto_now=True)
+    publicado = models.BooleanField(default=False)
+    categoria = models.ForeignKey(Categoria,on_delete=models.SET_NULL, null=True)
+    autor = models.ForeignKey(User,on_delete=models.SET_NULL, null=True)
+
